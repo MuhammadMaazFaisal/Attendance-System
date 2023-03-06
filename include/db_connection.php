5548e366151e4c429f8300f2a424aaf555602fda
Muhammad Maaz Faisal <73815553+MuhammadMaazFaisal@users.noreply.github.com>
2023-03-06 19:54:23 +0500

    added database credentials

--- include/db_connection.php
+++ include/db_connection.php
@@ -1,9 +1,9 @@
 <?php 
 // Database configuration
-$dbHost     = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName     = "attendance system";
+$dbHost     = "us-cdbr-east-06.cleardb.net";
+$dbUsername = "bf7ca6f7d79144";
+$dbPassword = "f2eef79d";
+$dbName     = "heroku_38d47f0bebcb01b";
 
 // Create database connection
 $conn = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
