5ad23948b52b4098ab488086ce884c6313b58e31
Muhammad Maaz Faisal <73815553+MuhammadMaazFaisal@users.noreply.github.com>
2023-03-06 19:57:56 +0500

    Update db_connection.php

--- include/db_connection.php
+++ include/db_connection.php
@@ -11,5 +11,7 @@ $conn = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-}else
+}else{
+    echo "connection established"
+}
 ?>
